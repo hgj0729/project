bc4f687057b345b7f37956f2cb87d266def15f8d
hgj0729 <hgj072969@gmail.com>
2026-08-12 09:30:20 +0900

    Add files via upload

--- formdate/js/script.js
+++ formdate/js/script.js
@@ -1,33 +1,27 @@
 const scriptURL =
-
-"https://script.google.com/macros/s/AKfycbxrkgknCLUr2w_6f52kNO22yl-S2J9ynOOOV3DP3LpGM9E8YUv74pf8EAo4lz9uD3wr/exec"; // 자신의 app스크립트 주소 링크 넣는 위치
+  "https://script.google.com/macros/s/AKfycbxrkgknCLUr2w_6f52kNO22yl-S2J9ynOOOV3DP3LpGM9E8YUv74pf8EAo4lz9uD3wr/exec";
 
 const form = document.forms["submit-to-google-sheet"];
-
 const msg = document.getElementById("msg");
 
-​
-
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-e.preventDefault();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+  })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
 
-fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
 
-.then((response) => {
-
-msg.innerHTML = "Message sent successfully";
-
-setTimeout(function () {
-
-msg.innerHTML = "";
-
-}, 5000);
-
-form.reset();
-
-})
-
-.catch((error) => console.error("Error!", error.message));
-
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      msg.innerHTML = "전송 중 오류가 발생했습니다.";
+    });
 });
