f907c64bbf8aae68b8bfdaa12bd9bcfb9c4176ea
hgj0729 <hgj072969@gmail.com>
2026-08-31 17:32:53 +0900

    Add files via upload

---
+++ build/config.js
@@ -0,0 +1,13 @@
+/*
+  FestOn 런타임 설정
+
+  1) 공공데이터포털 > 한국관광공사 국문 관광정보 서비스_GW 활용신청
+  2) 승인된 인증키를 아래 따옴표 안에 붙여넣기
+  3) Decoding 키 / Encoding 키 둘 다 자동 처리합니다.
+
+  예)
+  PUBLIC_DATA_API_KEY: "발급받은_인증키"
+*/
+window.APP_CONFIG = {
+  PUBLIC_DATA_API_KEY: "nwy3yX99FuAiOzZBy%2FFfva9B080mn85VkQCIKMZEOKkUvizNmMM4GkJD3ADXs5xApBt9Sf5rpfrqSHasnFqbVQ%3D%3D"
+};
